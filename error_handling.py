4361de2bdecf1e308714cde2b8e4f6239ee41edc
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-05-10 14:00:39 +0530

    Add files via upload

---
+++ error_handling.py
@@ -0,0 +1,15 @@
+"""
+Day 05
+Date: 25thth April 2022 monday
+Name: Bapatla sravan
+File Desc: error handling
+"""
+
+
+
+# print(3/0) # ZeroDivisionError
+# print(hh) # NameError
+# a="hello"
+# print(int(a)) # ValueError
+# b=4.5
+# print(b+"Hello") # TypeError
